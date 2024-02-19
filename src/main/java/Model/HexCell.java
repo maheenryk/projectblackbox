8eaf9429f904aec65726d54274447224274fbf51
rainebow <ramatla75@gmail.com>
2024-02-19 15:58:19 +0000

    hex cell class created

---
+++ src/main/java/Model/HexCell.java
@@ -0,0 +1,12 @@
+package Model;
+//Define hex cell in our board
+/*Properites of HexCell :
+ 1. position
+ 2.contains info about neighbouring cells usually each  heex has 6 neighbours except the edge hexes
+ 3.contains info about if atom is present or not
+
+ */
+
+
+public class HexCell {
+}
