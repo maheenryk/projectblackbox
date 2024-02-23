70fb46c4c4c18ee5aad07c940880f3a533a5022f
maheenryk <maheen.ahmed@ucdconnect.ie>
2024-02-23 23:43:04 +0000

    test

--- src/main/java/Model/ModelMain.java
+++ src/main/java/Model/ModelMain.java
@@ -1,13 +1,14 @@
 package Model;
 
 public class ModelMain {
-    /*testing to see Hashmap is mapped correctly.
+    //testing to see Hashmap is mapped correctly.
     public static void main(String[] args) {
         BlackBoxBoard blackBoxBoard = new BlackBoxBoard();
         blackBoxBoard.printBoard();
 
         BlackBoxBoard bbBoard = new BlackBoxBoard();
-        System.out.println(bbBoard.getBoardSize());*/
+        System.out.println(bbBoard.getBoardSize());
     }
+}
 
 
