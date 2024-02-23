b6f66b364b6fb099aa07001587ce86e2ff0ab3f9
rainebow <ramatla75@gmail.com>
2024-02-23 21:18:01 +0000

    updated hexcell claas master class

--- src/main/java/Model/Atom.java
+++ src/main/java/Model/Atom.java
@@ -1,46 +1,50 @@
 package Model;
-/* Atom Class:
-1.This will for now contain info about atoms position.
+/*Atom Class:
+1.this will for now contain info about atoms position reference to Point3D
+2.state management for later on ray hit tracking
+3.equals method
  */
 
 public class Atom {
+    //reference to co-ordinates
 
-    private final int x;
-    private final int y;
-    private final int z;
+    private final BlackBoxBoard.Point3D position;
 
 
-    public Atom(int x, int y, int z) {
-        if(x + y + z != 0){
-            throw  new IllegalArgumentException("Invalid co-ordinates");
-        }
-        this.x = x;
-        this.y = y;
-        this.z = z;
+
+    public Atom( BlackBoxBoard.Point3D position) {
+        //validation is in point3D
+
+        this.position = position;
     }
 
-    public int getX() {
-        return x;
+    public BlackBoxBoard.Point3D getPosition() {
+        return position;
     }
 
-    public int getY() {
-        return y;
-    }
-
-    public int getZ() {
-        return z;
-    }
 
     @Override
     public String toString() {
-        return "Atom{" +
-                "x=" + x +
-                ", y=" + y +
-                ", z=" + z +
-                '}';
+        // make sure  Point3D class has a proper toString implementation
+        return "Atom{ " + "position: " + position + "}";
     }
 
-    //will update as game logic progresses.
+
+    /*will update as game logic progresses.
+       1.such as how ray interacts with atom
+       2.state whether hit by ray
+       3.atom as has been guessed by the experimenter
+
+     */
+
+
+    /* storing of the atoms
+     1.using a hast set
+     2. good it has methods contains and add to prevent atom duplicates(prevents atoms beig added to cells that already contain atom) and to add atoms
+     3. we can check ray interactions  that happen to our atom super easily especially if we are using hashmap for our  board
+      */
+
+
 
 }
 
