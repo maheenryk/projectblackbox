4e81363129288f3f076fb754c9a39d532995bc52
Bokang <147839054+Bokang2@users.noreply.github.com>
2024-02-23 22:55:44 +0000

    Update HexCell.java

--- src/main/java/Model/HexCell.java
+++ src/main/java/Model/HexCell.java
@@ -2,80 +2,60 @@ package Model;
 
 /* Define hex cell in our board
    Properties of HexCell :
-    1.positions?
-    2.create a reference to atom class to set or check if hex cell ha an atom present
+    1.a reference to the Point3D (points of it co-ordinates)
+    2.a reference to atom class to set an atom with appropriate validation checks
+        >get the HexCell with the  atom details
+        >check if cell has atom
+        >a toString method to represent our HexCell object
 
  */
 
 
 public class HexCell {
-    /* 1.define instance variables
-       2. make them immutable, so they cannot be changed once set.
-    */
+    //reference to its co-ordinates
+    private  final BlackBoxBoard.Point3D points;
+    //create reference to atom
+    private Atom atom;
 
-    /*private final int x;
-      private final int y;
-      private final int z;
-      hexagon with atom present or not.
-      also final as once game starts twe cannot add or remove atoms once they have been set.
-    */
+    public HexCell(BlackBoxBoard.Point3D points) {
+        this.points = points;
+        //at the start all HexCell do not contain an atom
+        this.atom = null;
 
-    //create an instance variable of type atom.
-     private Atom atom;
+    }
 
-     public HexCell() {
-        //at the start all hex cells do not contain an atom
-         this.atom = null;
 
-     }
+    public boolean setAtom(Atom atom) {
+        //prevents atom being placed in a HexCell that already contains an atom so essentially prevents overwritting
+        if(this.atom == null){
+            this.atom = atom;
+            return true;
+        }
+        //HexCell already contains atom
+        return false;
 
-      //set atom
-      public void setAtom(Atom atom) {
-        this.atom = atom;
-      }
-      public boolean hasAtom() {
+    }
+    public Atom getAtom() {
+        // returns the Atom details like location if there's no atom in the cell it will be null
+        //will be beneficial when implementing ray logic
+        //will add more details as game progresses
+        return this.atom;
+    }
+    public boolean hasAtom() {
+
         return this.atom != null;
-      }
+    }
 
-      //returns a string rep of our HexCell
 
+    //returns a string rep of our HexCell
     @Override
     public String toString() {
-         String atomCheck;
-         if(this.atom == null){
-           atomCheck= "atom is not present";
-         }else{
-            atomCheck ="atom is present";
-         }
-         return  "HexCell; " + atomCheck;
+
+        if(hasAtom()){
+            return "HexCell{ " +"co-ordinates:" +points + ", atom:" +atom +"}";
+        }else{
+            return "HexCell{ " +"co-ordinates:" +points + ", atom: no atom is present"  +"}";
+        }
     }
-
-
-
-
-  /*  public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public int getZ() {
-        return z;
-    }
-
-    public boolean hasAtom() {
-        return hasAtom;
-
-    }
-
-      public String getCoOrdinates() {
-        return "[" + x + "," + y + "," + z + "]";
-    }
-    */
-
-
-
 
 }
