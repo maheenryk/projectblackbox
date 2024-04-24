0ee748c71ab390dd28237873184b05c4686384ed
rainebow <ramatla75@gmail.com>
2024-04-24 22:08:08 +0100

    updated player class

--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -4,63 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Class representing a player
- * Players can either be a Setter or Experimenter store in an enum
-
+ * layers track of their role themselves
+ * they just hand over the laptop to the other player when their round is done
+ * ui displays you are setter you are experimenter
  */
 
 public class Player {
-    // declare our enum to store role states
-
-    enum PlayerRole{
-        SETTER,
-        EXPERIMENTER
-    }
-
-    private PlayerRole role;
-    public Player(PlayerRole role){
-        this.role = role;
-    }
-
-    public PlayerRole getRole() {
-        return role;
-    }
-
-    @Override
-    public String toString() {
-        return "Player role : " + role;
+    //private String role;
+    //players name
+    private String name;
+    //score
+    private int score;
+    public Player(String name){
+        this.name = name;
+       // this.role = role;
+        this.score =0;
     }
 
 
-    /*private PlayerRole role;
-      //tracks score for player
-     private int score = 0;
-      // the number of ray markers placed by experimenter
-     private int rayMarkersUsed = 0;
-     private List<BlackBoxBoard.Point3D> atomsPlaced = new ArrayList<>();
-      // reference to Blackboxboard
-     private BlackBoxBoard board;
-      // a ray list to store experimenters rays
-     private  List<Ray> rays = new ArrayList<>();
-      //constructor
-   public Player(PlayerRole role, BlackBoxBoard board){
-       this.role = role;
-       this.board = board;
+    // public String getRole() {
+     //   return role;
+    //}
+
+
+    //get players name
+    public String getName() {
+        return this.name;
+    }
+
+    //get the players score
+    public int getScore() {
+        return this.score;
+    }
+
+
+    //method to get their score at end of game
+    public void updateScore(int newScore) {
+        this.score += newScore;
     }
 
 
 
 
-    /**
-     *  Methods to be implemented :
-     *  CalculateScore
-     *  sendRay
-     *  Initialise board (set atoms -> setter)
-     *
-     *
-     *
-     *
-     * */
+
 
 
 }
