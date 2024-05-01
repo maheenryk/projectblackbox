cfe384d613e245f53085fa60fb6d474a591e935e
rainebow <ramatla75@gmail.com>
2024-05-01 18:35:02 +0100

    saving and loading scores

--- src/main/java/Controller/GameState.java
+++ src/main/java/Controller/GameState.java
@@ -1,5 +1,6 @@
 package Controller;
 import Model.BlackBoxBoard;
+import Model.Player;
 import javafx.geometry.Point2D;
 import utils.ReadyButtonClickedListener;
 
@@ -7,18 +8,34 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+//import utils.SaveScore;
 
 
 public class GameState implements ReadyButtonClickedListener {
     private final List<Point2D> setterAtoms = new ArrayList<>(); //list to store the setter atom positions
     private final List<Point2D> experimenterAtoms = new ArrayList<>(); //list to store experimenter atoms.
 
+   // private Map<String, Integer> scores = new HashMap<>();  // Store player scores mapped to  player names.
 
+   // private SaveScore scoreSave = new SaveScore("SavedScores.properties");  // Instance of SaveScore
+
+   /* public void addScore(String playerName, int score) {
+        // Adds or updates the score for a player in the map and immediately saves it to the properties file.
+        scores.put(playerName, score);
+        scoreSave.saveScore(playerName, score);  // Save immediately
+    }
+
+    public int getScore(String playerName) {
+        // gets the score for a player from the map. If no score is found, returns 0 as default.
+        return scores.getOrDefault(playerName, 0);
+    }
+*/
 
     public void startNewGame() {
-         //other new game code here (future sprint)
+        //other new game code here (future sprint)
         System.out.println("New Game!");
     }
+
     private boolean isReadyClicked = false; //set ready button click flag to false.
 
     @Override
@@ -27,7 +44,9 @@ public class GameState implements ReadyButtonClickedListener {
             isReadyClicked = true;
             System.out.println("READY button clicked");
             //call method for beginning experimenter turn here
-            //maybe reset flag here.
+            //reset flag here.
+
+
         }
     }
 
@@ -35,6 +54,7 @@ public class GameState implements ReadyButtonClickedListener {
         this.setterAtoms.clear();
         this.setterAtoms.addAll(positions);
     }
+
     public List<Point2D> getSetterAtomPositions() {
         return this.setterAtoms;
     }
@@ -43,13 +63,13 @@ public class GameState implements ReadyButtonClickedListener {
         this.experimenterAtoms.clear();
         this.experimenterAtoms.addAll(positions);
     }
+
     public List<Point2D> getExperimenterAtoms() {
         return this.experimenterAtoms;
     }
 
 
-
-    public static Map<String, Integer>  calcScore(List<Point2D> setterAtoms, List<Point2D> experimenterAtoms, BlackBoxBoard board) {
+    public static Map<String, Integer> calcScore(List<Point2D> setterAtoms, List<Point2D> experimenterAtoms, BlackBoxBoard board) {
         int correctAtoms = setterAtoms.size();
 
         // only keep in experimenter atoms list the atoms that match with setter's list
@@ -75,14 +95,24 @@ public class GameState implements ReadyButtonClickedListener {
         return results;
 
     }
+    //method to end game to save score
+ /*   public void resetGame() {
+        // Clears all game data to start a new game.
+        setterAtoms.clear();
+        experimenterAtoms.clear();
+        //scores.clear();  // Optionally clear scores if needed
+    }*/
+
 
     //method for beginning experimenter turn.
-    public void resetReadyClicked() {//resetting flag from outside
+    public void resetReadyClicked () {//resetting flag from outside
         isReadyClicked = false;
     }
+
+
 }
 
-    //method for comparing the atoms of experimnter and setter for scoring purposes:
+
 
 
 
--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -4,44 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * layers track of their role themselves
+ * players keep track of their role themselves
  * they just hand over the laptop to the other player when their round is done
  * ui displays you are setter you are experimenter
  *
  */
 
 public class Player {
-    //private String role;
+
     //players name
     private String name;
     //score
-    private int score;
+  //  private int score;
     public Player(String name){
         this.name = name;
-       // this.role = role;
-        this.score =0;
+
     }
-
-
-    // public String getRole() {
-     //   return role;
-    //}
 
 
     //get players name
     public String getName() {
         return this.name;
-    }
-
-    //get the players score
-    public int getScore() {
-        return this.score;
-    }
-
-
-    //method to get their score at end of game
-    public void updateScore(int newScore) {
-        this.score += newScore;
     }
 
 
