f2441fb1c2f09ecf726384ad7de91cfa1bc7dc91
rainebow <ramatla75@gmail.com>
2024-05-05 05:28:38 +0100

    javadoc for gamestate and blackboxboard

--- src/main/java/Controller/GameState.java
+++ src/main/java/Controller/GameState.java
@@ -8,52 +8,96 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Manages the state and the flow of the game, handling interactions such as
+ * game initialisation, atom placement, and scoring.
+
+ */
+
+
 
 public class GameState implements ReadyButtonClickedListener {
     private final List<Point2D> setterAtoms = new ArrayList<>(); //list to store the setter atom positions
     private final List<Point2D> experimenterAtoms = new ArrayList<>(); //list to store experimenter atoms.
 
 
-
+    /**
+     * Initialises a new game, setting up the needed game elements.
+     */
     public void startNewGame() {
-        //other new game code here (future sprint)
         System.out.println("New Game!");
     }
     private boolean isReadyClicked = false; //set ready button click flag to false.
 
+    /**
+     * Handles the ready button click. Sets the flag to true on the first click.
+     */
+
+
     @Override
     public void onReadyClicked() {
-        if (!isReadyClicked) { //action on first click
+        if (!isReadyClicked) {
             isReadyClicked = true;
             System.out.println("READY button clicked");
-            //call method for beginning experimenter turn here
-            //maybe reset flag here.
         }
     }
 
+    /**
+     * Sets the positions of atoms placed by the setter.
+     *
+     * @param positions A list of Point2D of the positions  of the setter's atoms.
+     */
     public void setSetterAtomPositions(List<Point2D> positions) {
         this.setterAtoms.clear();
         this.setterAtoms.addAll(positions);
     }
+
+    /**
+     * Gets the positions of atoms placed by the setter.
+     *
+     * @return A list of Point2D of  the positions of the setter's atoms.
+     */
     public List<Point2D> getSetterAtomPositions() {
         return this.setterAtoms;
     }
 
+    /**
+     * Sets the positions of atoms placed by the experimenter.
+     *
+     * @param positions A list of Point2D of  the positions of the experimenter's atoms.
+     */
     public void setExpAtomPositions(List<Point2D> positions) {
         this.experimenterAtoms.clear();
         this.experimenterAtoms.addAll(positions);
     }
+
+    /**
+     * Gets the positions of atoms placed by the experimenter.
+     *
+     * @return A list of Point2D of the positions of the experimenter's atoms.
+     */
+
     public List<Point2D> getExperimenterAtoms() {
         return this.experimenterAtoms;
     }
 
 
 
+
+    /**
+     * Calculates the score based on the placement of atoms by the setter and experimenter.
+     * Scores are calculated based on correct placements and penalties for incorrect placements.
+     *
+     * @param setterAtoms The list of Point2D from the setter.
+     * @param experimenterAtoms The list of Point2D from the experimenter that needs to match the setter's list.
+     * @param board The black box board is  used to track other game logic like rays.
+     * @return A map containing scoring details including correct atom placements and total score.
+     */
     public static Map<String, Integer>  calcScore(List<Point2D> setterAtoms, List<Point2D> experimenterAtoms, BlackBoxBoard board) {
         int correctAtoms = setterAtoms.size();
 
         // only keep in experimenter atoms list the atoms that match with setter's list
-        experimenterAtoms.retainAll(setterAtoms);
+          experimenterAtoms.retainAll(setterAtoms);
         int incorrectAtoms = (correctAtoms - experimenterAtoms.size());
         int score = incorrectAtoms * 5;
 
@@ -77,7 +121,7 @@ public class GameState implements ReadyButtonClickedListener {
         System.out.println("Total ray markers: " + board.getRayMarkers());
         score += BlackBoxBoard.rayMarkers;
 
-        Map<String, Integer> results = new HashMap<>(); //map for storing results
+        Map<String, Integer> results = new HashMap<>();
         results.put("score", score);
         //results.put("incorrectAtoms", incorrectAtoms);
         results.put("correctAtoms", correctlyPlacedAtoms);
@@ -88,13 +132,15 @@ public class GameState implements ReadyButtonClickedListener {
 
     }
 
-    //method for beginning experimenter turn.
+    /**
+     * Resets the ready button click flag.
+     */
     public void resetReadyClicked() {//resetting flag from outside
         isReadyClicked = false;
     }
 }
 
-//method for comparing the atoms of experimnter and setter for scoring purposes:
+
 
 
 
--- src/main/java/Model/BlackBoxBoard.java
+++ src/main/java/Model/BlackBoxBoard.java
@@ -3,19 +3,36 @@ import javafx.geometry.Point2D;
 
 import java.util.*;
 
+/**
+ * Represents the black box game board for the game. This class manages the
+ * hex cells, atom placements, and game board.
+ */
+
 public class BlackBoxBoard {
 
+    /**
+     * Static list to track randomly placed atoms on the board.
+     */
     public static List<Point3D> randomAtoms = new ArrayList<>();
 
-    /* creating custom Point3D class to make connecting view (JavaFX) and model efficient.
-    co-ord mapping shouldn't be done only in JavaFX using Point3D as goes against layer abstraction.
-     -> therefore, custom Point3D class holding 3-d co-ords to suit cubic hexagonal mapping system.*/
+    /**
+     * Represents a 3D coordinate  on the hexagonal grid.
+     * Ensures that the sum of coordinates (x, y, z) equals zero to validate  placement on the grid.
+     */
     public static class Point3D {
         public final int x;
         public final int y;
         public final int z;
 
-        //Point3D constructor
+        /**
+         * Constructs a new Point3D with specified coordinates.
+         *
+         * @param x The x coordinate.
+         * @param y The y coordinate.
+         * @param z The z coordinate.
+         * @throws IllegalArgumentException if the sum of x, y, and z does not equal zero.
+         */
+
         public Point3D(int x, int y, int z) {
             if (x + y + z != 0) {
                 throw new IllegalArgumentException("Invalid Co-Ordinates");
@@ -25,9 +42,6 @@ public class BlackBoxBoard {
             this.z = z;
         }
 
-        /*
-        -- [[not sure if required anymore]]. --
-        must override to ensure distinct hashcode for unique combinations of x, y, z. */
        @Override
         public int hashCode() {
             return java.util.Objects.hash(x, y, z);
@@ -41,7 +55,7 @@ public class BlackBoxBoard {
             return x == point3D.x && y == point3D.y && z == point3D.z;//checking fields in objects for equivalence
         }
 
-        //hashcode is printed without overriding, so toString method ust be overridden for Point3D object.
+
         @Override
         public String toString() {
             return "Point3D(" + "x=" + x + ", y=" + y + ", z=" + z + ')';
@@ -52,11 +66,13 @@ public class BlackBoxBoard {
     //defining board as Map structure with co-ords as key and hex cell as value.
     private final Map<Point3D, HexCell> board;
 
-    //BB-board constructor
+    /**
+     * Initialises the BlackBoxBoard with a set number of random atoms.
+     */
     public BlackBoxBoard() {
         this.board = new LinkedHashMap<>();
-        initializeBoard();  // Initialise the board first to ensure all cells are set up
-        placeRandomAtoms(6);  // Then place random atoms
+        initializeBoard();
+        placeRandomAtoms(6);
     }
 
 
@@ -72,7 +88,9 @@ public class BlackBoxBoard {
             new Point3D(-4, 2, 2),  new Point3D(-4, 1, 3),  new Point3D(-4, 0, 4),
             new Point3D(-3, -1, 4), new Point3D(-2, -2, 4), new Point3D(-1, -3, 4));
 
-    //nested for loops iterate within [-4, 4] range and fill hashmap with key-value pairs.
+    /**
+     * Initialises the hex cells within the board.
+     */
     private void initializeBoard() {
         for (int y = -4; y <= 4; y++) /* y prints as the directional invariable so that the cells
         are printed top-down, row by row*/
@@ -87,7 +105,9 @@ public class BlackBoxBoard {
             }
     }
 
-    //method to print board entries (all key + value pairs)
+    /**
+     * Outputs the current state of the board including all hex cells.
+     */
     public void printBoard() {
         System.out.println("HashMap Contents:");
         for (Map.Entry<Point3D, HexCell> entry : board.entrySet()) {
@@ -95,7 +115,12 @@ public class BlackBoxBoard {
         }
     }
 
-    //method to get the size of the board (no. of hex cells). [needed for testing]
+    /**
+     * Returns the size of the board in terms of number of hex cells.
+     *
+     * @return The number of hex cells on the board.
+     */
+
     public int getBoardSize() {
         return this.board.size();
     }
@@ -108,22 +133,46 @@ public class BlackBoxBoard {
         return this.board.entrySet().iterator();
     }
 
-
+    /**
+     * Validates if the provided coordinates are within the valid range for the board.
+     *
+     * @param x The x coordinate.
+     * @param y The y coordinate.
+     * @param z The z coordinate.
+     * @return true if the coordinates are within the board's limits, false otherwise.
+     */
     public boolean isValidCoordinate(int x, int y, int z) {
         return (x >= -4 && x <= 4) && (y >= -4 && y <= 4) && (z >= -4 && z <= 4);
     }
+
+
 
     public static int rayCount = 0;
 
     public static int rayMarkers = 0;
 
+    /**
+     * Gets the count of rays that have been fired.
+     *
+     * @return the ray count
+     */
     public int getRayCount() {
         return rayCount;
     }
 
+    /**
+     * Gets the number of ray markers on the board.
+     * @return the ray markers count
+     */
     public int getRayMarkers() {
         return rayMarkers;
     }
+
+    /**
+     * Places atoms specified by the setter on the board.
+     *  These atoms are placed only if the cell at the coordinate does not already have an atom.
+     * @param setterAtomList a list of Point3D with the coordinates for placing an atom.
+     */
 
     public void placeSetterAtoms(List<Point3D> setterAtomList) { //updating the board with the setter's Atoms.
         for (Point3D point3d : setterAtomList) {
@@ -134,13 +183,17 @@ public class BlackBoxBoard {
         }
     }
 
-
+    /**
+     * Places an atom at a specific point on the board. It also assigns CIPoints.
+     * around the placed atom if they fall within valid coordinates.
+     * @param point The point on the board where the atom is to be placed.
+     */
     public void placeAtom(Point3D point) {
         int CIx = point.x;
         int CIy = point.y;
         int CIz = point.z;
 
-        // Place an atom at this point if it doesn't already have one
+
         HexCell hexCell = board.get(point);
         if (!hexCell.hasAtom()) {
             hexCell.setAtom(new Atom());
@@ -180,7 +233,12 @@ public class BlackBoxBoard {
         }
     }
 
-    //method to place our atoms in cells randomly while staying within boards range  at the start of game
+    /**
+     * Places a specific number of atoms randomly on the board.
+     *
+     * @param numberOfAtoms The number of atoms to place.
+     */
+
     public void placeRandomAtoms(int numberOfAtoms) {
         // create a list from the keys of the board HashMap, which are the valid positions
         List<Point3D> validPoints = new ArrayList<>(board.keySet());
@@ -209,8 +267,12 @@ public class BlackBoxBoard {
         }
     }
 
-
-    // method to retrieve HexCell to use in ray class when interacts with board
+    /**
+     * Gets a HexCell from the board based on a specific point.
+     *
+     * @param point The point where the HexCell is located.
+     * @return The HexCell at the specified point.
+     */
     public HexCell getCell(BlackBoxBoard.Point3D point) {
         return this.board.get(point);
     }
